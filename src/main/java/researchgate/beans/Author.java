f761ac6aa74592ee3c4142d67ea252039c4c87f3
mrri <marouane.ourdou@edu.uiz.ac.ma>
2021-05-16 16:13:56 +0100

    added beans

---
+++ src/main/java/researchgate/beans/Author.java
@@ -0,0 +1,81 @@
+package researchgate.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Author{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idAuthor;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String email;
+    private String address;
+    
+    @ManyToOne
+    @JoinColumn(name="idAffiliation")
+    private Affiliation affiliation;
+
+    public Long getIdAuthor() {
+        return idAuthor;
+    }
+
+    public void setIdAuthor(Long idAuthor) {
+        this.idAuthor = idAuthor;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Affiliation getAffiliation() {
+        return affiliation;
+    }
+
+    public void setAffiliation(Affiliation affiliation) {
+        this.affiliation = affiliation;
+    }
+}
